e7b9f675210721cf37d6858484e1823a178fba49
Hanif Ulunnuha Hidayat <ulunnuha.h@gmail.com>
2022-07-24 17:16:47 +0700

    [Han] Parallax finish

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import PageOne from './pageOne';
+import { ParallaxProvider } from 'react-scroll-parallax';
 
 export default function App() {
     return(
-        <div>
-          <PageOne/>
-        </div>
+        <ParallaxProvider>
+            <PageOne/>
+            <div className='page-2 w-100' style={{"height":"100vh"}}>
+            <img alt='imag' className='el el-8 position-absolute' src={require("./Images/Elemen/el-8.png")}></img>
+            </div>
+        </ParallaxProvider>
     );
 }
--- src/pageOne.js
+++ src/pageOne.js
@@ -1,23 +1,18 @@
 import React from 'react'
-
-
+import {useParallax } from 'react-scroll-parallax';
 
 export default function PageOne() {
+
     return(
-        <div className='container position-relative bgStyle' style={{backgroundImage: `url(${require("./Images/Elemen/el-1.jpg")})`}}>
-            <img className='el-1 bgStyle' src={require("./Images/Elemen/el-6.png")} alt='gunung-1'/>
-            <div className='el-2'></div>
-            <div className='el-3'></div>
-            <div className='el-4'></div>
-            <div className='el-5'></div>
-            <div className='el-6'></div>
-            <div className='el-7'></div>
-            <div className='el-8'></div>
-            <div className='el-9'></div>
-            <div className='el-10'></div>
-            <div className='el-12'></div>
-            <div className='el-12'></div>
-            <div className='el-13'></div>
+        <div className='container-fluid m-0 p-0'  style={{"height":"140vh",zIndex:"-100"}}>
+            <img ref={useParallax({speed : -60}).ref} className='el el-1 position-absolute' src={require("./Images/Elemen/el-1.png")} style={{zIndex:"-1"}} alt="pic"></img>
+            <img ref={useParallax({speed : -35}).ref} className='el el-2 position-absolute' src={require("./Images/Elemen/el-2.png")} style={{zIndex:"-1"}} alt="pic"></img>
+            <img ref={useParallax({speed : -30}).ref} className='el el-3 position-absolute' src={require("./Images/Elemen/el-3.png")} style={{zIndex:"-1"}} alt="pic"></img>
+            <img ref={useParallax({speed : -25}).ref} className='el el-4 position-absolute' src={require("./Images/Elemen/el-4.png")} style={{zIndex:"-1"}} alt="pic"></img>
+            <img ref={useParallax({speed : -100}).ref} className='el-logo position-absolute' src={require("./Images/LOGO-IF-ELSE.png")} style={{zIndex:"-1"}} alt="pic"></img>
+            <img ref={useParallax({speed : -20 }).ref} className='el el-5 position-absolute' src={require("./Images/Elemen/el-5.png")} style={{zIndex:"-1"}} alt="pic"></img>
+            <img ref={useParallax({speed : -10}).ref} className='el el-6 position-absolute' src={require("./Images/Elemen/el-6.png")} style={{zIndex:"-1"}} alt="pic"></img>
+            <img ref={useParallax({speed : -2}).ref} className='el el-7 position-absolute' src={require("./Images/Elemen/el-7.png")} style={{zIndex:"-1"}} alt="pic"></img>
         </div>
     )
 }
